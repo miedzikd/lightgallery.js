4e937d39e1d8bab8b6e4948d8c061ea146b4cf8c
Daniel Miedzik <daniel.miedzik@w3center.pl>
2019-06-21 13:33:21 +0200

    Copy changes from src

--- dist/js/lightgallery.js
+++ dist/js/lightgallery.js
@@ -362,9 +362,37 @@
         return this;
     }
 
-    Plugin.prototype.init = function () {
+Plugin.prototype.requestFullscreen = function() {
+    console.log("requestFullscreen ");
+    var el = document.documentElement;
+    if (el.requestFullscreen) {
+        el.requestFullscreen();
+    } else if (el.msRequestFullscreen) {
+        el.msRequestFullscreen();
+    } else if (el.mozRequestFullScreen) {
+        el.mozRequestFullScreen();
+    } else if (el.webkitRequestFullscreen) {
+        el.webkitRequestFullscreen();
+    }
+};
 
-        var _this = this;
+Plugin.prototype.exitFullscreen = function() {
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen();
+    } else if (document.mozCancelFullScreen) {
+        document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen();
+    }
+};
+
+Plugin.prototype.init = function() {
+    
+    var _this = this;
+    
+    _this.requestFullscreen();
 
         // s.preload should not be more than $item.length
         if (_this.s.preload > _this.items.length) {
